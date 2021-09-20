faa422bc7122ff60e91acf3c8ab1b85d091c9fec
hsg0520 <hwangsg87@naver.com>
2021-09-20 16:41:53 +0900

    aaa

--- src/test/java/com/hsg/order/OrderApplicationTests.java
+++ src/test/java/com/hsg/order/OrderApplicationTests.java
@@ -1,5 +1,9 @@
 package com.hsg.order;
 
+// github code
+// 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
+
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
