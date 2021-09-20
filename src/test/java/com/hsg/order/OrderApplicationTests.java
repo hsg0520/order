60ca2859aefea04cc2d0908db5cdc8c2aec5059b
polaris1833 <polaris1833@naver.com>
2021-09-20 17:21:34 +0900

    수정

--- src/test/java/com/hsg/order/OrderApplicationTests.java
+++ src/test/java/com/hsg/order/OrderApplicationTests.java
@@ -2,7 +2,7 @@ package com.hsg.order;
 
 // github code
 // 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
-
+// ㅈ 같다
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
